23369d28427e9789a8e15a55ccd0bbee30784492
infernalslam <infernal-slam@hotmail.com>
2016-11-06 17:02:40 +0700

    success

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === "tak1234") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge'])
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.")
-    res.sendStatus(403)          
-  }  
+  if (req.query['hub.verify_token'] === '1234') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
 })
 
 app.listen(app.get('port'), function () {
