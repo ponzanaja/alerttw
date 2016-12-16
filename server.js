3acc79f28e0cd5bb3c634f5432c0c1f5d2c8c551
ponzanaja <ponza2539@hotmail.com>
2016-12-16 07:20:17 +0700

    edit send

--- server.js
+++ server.js
@@ -276,8 +276,9 @@ userInfo.forEach( function (data,index) {
 function checkSend () {
     console.log('checking Sending status')
 userInfo.forEach( function (data,index) {
-    console.log('process sending status'+index)
+    console.log('process sending status: '+index)
   data.follower.forEach( function (follow,index2) {
+      console.log('process sending status phase2 :'+index)
     if(data.follower.live){
         sendTextMessage(data.UID,'ช่อง '+data.follower.name+' ที่คุณติดตามไว้ Live แล้วสามารถรับเข้าไปรับชมได้' )
         firebase.database().ref('users/' + data.id +'/follower/'+index2).update({
