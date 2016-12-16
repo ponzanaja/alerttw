4d036e8ed7d217d7eae0d89b6c7124fe7f65b9db
ponzanaja <ponza2539@hotmail.com>
2016-12-16 10:42:43 +0700

    edit bug

--- server.js
+++ server.js
@@ -330,7 +330,7 @@ function showList (senderID) {
 
       userIn.follower.forEach( function (data,index){
           sendTextMessage(senderID,' '+data.name+'\n')
-          if(index === follower.length){
+          if(index === data.length){
             sendTextMessage(senderID,'คุณสามารถลบ Channel ที่ไม่ต้องการได้โดยการพิมพ์ชื่อ ![ชื่อที่ต้องการลบ] เช่น !eiei')
           }
         })
